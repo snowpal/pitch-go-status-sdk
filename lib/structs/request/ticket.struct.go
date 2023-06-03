1a1e9d58285f175e09aa58ad1599c244b8daf868
AnishGhumara <khatri.anis@gmail.com>
2023-06-03 09:31:11 +0530

    fix: fixed structs

--- lib/structs/request/ticket.struct.go
+++ lib/structs/request/ticket.struct.go
@@ -6,8 +6,22 @@ type AddSessionsReqBody struct {
 	Sessions []SessionReqBody `json:"sessions"`
 }
 
+type SessionReqBody struct {
+	SequenceId int                       `json:"sequenceId"`
+	Tickets    []TicketReqBody           `json:"tickets"`
+	OtherItems []SessionOtherItemReqBody `json:"otherItems"`
+}
+
 type StatusSessionOtherItemsReqBody struct {
 	OtherItems []SessionOtherItemReqBody `json:"otherItems"`
+}
+
+type SessionOtherItemReqBody struct {
+	Title         string                `json:"title"`
+	Type          string                `json:"type"`
+	TimeSpent     float32               `json:"timeSpent"`
+	PairedMembers []common.PairedMember `json:"pairedWith"`
+	Comment       string                `json:"comment"`
 }
 
 type AddSessionTicketsReqBody struct {
@@ -27,6 +41,15 @@ type SessionTicketReqBody struct {
 
 type AddTicketsReqBody struct {
 	Tickets []TicketReqBody `json:"tickets"`
+}
+
+type TicketReqBody struct {
+	TicketID    string `json:"ticketID"`
+	Type        string `json:"type"`
+	Url         string `json:"url"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	Points      int    `json:"points"`
 }
 
 type TicketParam struct {
